000bcaa39ecb23027759153e8e635af5582ac994
Rubio Calin <Miguel.Rubio@seat.es>
2023-02-06 16:03:25 +0100

    refactor: Added a print statement in the end of the code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -195,7 +195,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+        print(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
